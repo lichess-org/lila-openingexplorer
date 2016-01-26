a61616a61b2a2d243ddd83f5e7829a4baef5dd2b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-01-26 15:53:35 +0100

    Experiment with scalachess and json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,12 +4,24 @@ import play.api.libs.json._
 import play.api._
 import play.api.mvc._
 
+import chess._
+import chess.format.Forsyth
+import chess.variant._
+
 class Application extends Controller {
 
   def index = Action { req =>
-    Ok(Json.obj(
-      "hello" -> "world"
-    )) as JSON
+    val position = "8/3k4/2q5/8/8/K1B5/8/8 w - -"
+
+    val situation = Forsyth << position
+
+    val moves = situation match {
+      case Some(s) => s.moves.values.flatten.map {
+        case (move) => Json.toJson(move.toString)
+      }
+    }
+
+    Ok(JsArray(moves.toSeq));
   }
 
 }
