4c27d71a0949d889dd6903b2ebf948238a6a1932
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-02-01 23:18:35 +0100

    Parse PGNs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -19,7 +19,7 @@ import chess.format.Forsyth
 import chess.variant._
 import chess.Hash
 
-import lila.openingexplorer.Entry
+import lila.openingexplorer.{Entry, GameRef}
 
 @Singleton
 class Application @Inject() (
@@ -44,6 +44,11 @@ class Application @Inject() (
     }
   }
 
+  private def merge(situation: Situation, entry: Entry) = {
+    val merged = probe(situation).combine(entry)
+    db.set(hash(situation), merged.pack)
+  }
+
   def index() = Action { implicit req =>
     val fen = req.queryString get "fen" flatMap (_.headOption)
     val situation = fen.flatMap(Forsyth << _)
@@ -51,6 +56,33 @@ class Application @Inject() (
     situation.map(probe(_)) match {
       case Some(entry) => Ok(entry.totalGames.toString)
       case None        => BadRequest("valid fen required")
+    }
+  }
+
+  def winner(game: chess.format.pgn.ParsedPgn) = {
+    game.tag("Result") match {
+      case Some("1-0") => Some(Color.White)
+      case Some("0-1") => Some(Color.Black)
+      case _           => None
+    }
+  }
+
+  def put() = Action { implicit req =>
+    val start = System.currentTimeMillis
+
+    val textBody = new String(req.body.asRaw.flatMap(_.asBytes()).getOrElse(Array.empty), "UTF-8")
+    println(textBody)
+    println("hello")
+    val parsed = chess.format.pgn.Parser.full(textBody)
+
+    parsed match {
+      case scalaz.Success(game) =>
+        val gameRef = new GameRef("zzzzzzzz", 3000, winner(game))
+        val entry = Entry.fromGameRef(gameRef)
+
+        Ok("thanks. time taken: " ++ (System.currentTimeMillis - start).toString ++ " ms")
+      case scalaz.Failure(e) =>
+        BadRequest(e.toString)
     }
   }
 
