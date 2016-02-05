eb06c3212027a836bebc7bb2b330d7bda718f953
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2016-02-05 19:20:11 +0100

    Tool to get an overview of a database

---
+++ util/stat.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+#include <kcpolydb.h>
+
+long estimate_a(long *pack) {
+  return (
+    4 + pack[0] * 8 +
+    4 + 1 + pack[1] * 8 * 5 +
+    4 + 1 + pack[2] * 8 * 5 + 3 * 11 * 1 +
+    4 + 1 + pack[3] * 8 * 5 + 3 * 11 * 2 +
+    4 + 1 + pack[4] * 8 * 5 + 3 * 11 * 4 +
+    4 + 1 + pack[5] * 8 * 5 + 3 * 11 * 6
+  );
+}
+
+long estimate_b(long *pack) {
+  return (
+    4 + pack[0] * 9 +
+    4 + 1 + pack[1] * 9 * 5 +
+    4 + 1 + pack[2] * 9 * 5 + 4 * 2 * 3 * 8 * 1 +
+    4 + 1 + pack[3] * 9 * 5 + 4 * 2 * 3 * 8 * 2 +
+    4 + 1 + pack[4] * 9 * 5 + 4 * 2 * 3 * 8 * 4 +
+    4 + 1 + pack[5] * 9 * 5 + 4 * 2 * 3 * 8 * 6
+  );
+}
+
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+      std::cout << "Usage: stat <dbfile.kct>" << std::endl;
+      return 1;
+    }
+
+    kyotocabinet::TreeDB db;
+
+    if (!db.open(argv[1], kyotocabinet::TreeDB::OREADER)) {
+      std::cout << "Could not open database." << std::endl;
+      return 2;
+    }
+
+    std::auto_ptr<kyotocabinet::TreeDB::Cursor> cur(db.cursor());
+    cur->jump();
+
+    std::string key, value;
+
+    long pack[] = {0, 0, 0, 0, 0, 0};
+
+    while (cur->get(&key, &value, true)) {
+        if (value.size() == 8) {
+            pack[0]++;
+        } else {
+            pack[value.at(0)]++;
+        }
+    }
+
+    std::cout << "Pack:" << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << i << "|" << pack[i] << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Estimates (in bytes):" << std::endl;
+
+    std::cout << "A: " << estimate_a(pack) << std::endl;
+    std::cout << "B: " << estimate_b(pack) << std::endl;
+    std::cout << "---" << std::endl;
+    std::cout << "B/A: " << ((double)estimate_b(pack)/estimate_a(pack)) << std::endl;
+
+    return 0;
+}
