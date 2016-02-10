5dd7fece5b04e99ee57306d637af825ff78eb660
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2016-02-10 10:38:54 +0100

    Make stat tool polymorphic

--- util/stat.cc
+++ util/stat.cc
@@ -3,42 +3,23 @@
 
 #include <kcpolydb.h>
 
-/* long estimate_a(long *pack) {
-  return (
-    4 + pack[0] * 8 +
-    4 + 1 + pack[1] * 8 * 5 +
-    4 + 1 + pack[2] * 8 * 5 + 3 * 11 * 1 +
-    4 + 1 + pack[3] * 8 * 5 + 3 * 11 * 2 +
-    4 + 1 + pack[4] * 8 * 5 + 3 * 11 * 4 +
-    4 + 1 + pack[5] * 8 * 5 + 3 * 11 * 6
-  );
-}
-
-long estimate_b(long *pack) {
-  return (
-    4 + pack[0] * 9 +
-    4 + 1 + pack[1] * 9 * 5 +
-    4 + 1 + pack[2] * 9 * 5 + 4 * 2 * 3 * 8 * 1 +
-    4 + 1 + pack[3] * 9 * 5 + 4 * 2 * 3 * 8 * 2 +
-    4 + 1 + pack[4] * 9 * 5 + 4 * 2 * 3 * 8 * 4 +
-    4 + 1 + pack[5] * 9 * 5 + 4 * 2 * 3 * 8 * 6
-  );
-} */
-
 int main(int argc, char **argv) {
     if (argc <= 1) {
-      std::cout << "Usage: stat <dbfile.kct>" << std::endl;
-      return 1;
-    }
-
-    kyotocabinet::TreeDB db;
-
-    if (!db.open(argv[1], kyotocabinet::TreeDB::OREADER)) {
-      std::cout << "Could not open database." << std::endl;
+      std::cout << "Usage: stat <dbfile>" << std::endl;
+      std::cout << "Shows the distribution of the different pack formats." << std::endl;
       return 2;
     }
 
-    std::auto_ptr<kyotocabinet::TreeDB::Cursor> cur(db.cursor());
+    kyotocabinet::PolyDB db;
+
+    std::cout << "Waiting for read lock ..." << std::endl;
+
+    if (!db.open(argv[1], kyotocabinet::PolyDB::OREADER)) {
+      std::cout << "Could not open database." << std::endl;
+      return 1;
+    }
+
+    std::auto_ptr<kyotocabinet::PolyDB::Cursor> cur(db.cursor());
     cur->jump();
 
     std::string key, value;
@@ -53,7 +34,14 @@ int main(int argc, char **argv) {
         if (value.size() == 8) {
             pack[0]++;
         } else {
-            pack[value.at(0)]++;
+            char c = value.at(0);
+
+            if (0 <= c && c <= 6) {
+                pack[c]++;
+            } else {
+                std::cout << "Error: Unknown pack format: " << c << std::endl;
+                return 1;
+            }
         }
 
         if (total % 50000 == 0) {
@@ -68,12 +56,6 @@ int main(int argc, char **argv) {
     }
 
     std::cout << "Unique positions: " << total << std::endl;
-
-    /* std::cout << std::endl;
-
-    std::cout << "Scheme A: " << estimate_a(pack) << " bytes" << std::endl;
-    std::cout << "Scheme B: " << estimate_b(pack) << " bytes" << std::endl;
-    std::cout << "B/A: " << ((double)estimate_b(pack)/estimate_a(pack)) << std::endl; */
 
     return 0;
 }
