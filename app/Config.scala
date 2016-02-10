4b1d4b9cb79704bcc683db5fa71a29117ba2a836
Thibault Duplessis <thibault.duplessis@gmail.com>
2016-02-10 20:28:30 +0700

    Config.explorer.lichess(variant).maxPlies

--- app/Config.scala
+++ app/Config.scala
@@ -3,6 +3,8 @@ package lila.openingexplorer
 import com.github.kxbmap.configs.syntax._
 import com.typesafe.config.ConfigFactory
 
+import chess.variant._
+
 object Config {
 
   val explorer = ConfigFactory.load.get[Explorer]("explorer")
@@ -24,13 +26,26 @@ object Config {
     pageSize: Long)
 
   case class Lichess(
-    standard: Domain,
-    chess960: Domain,
-    kingOfTheHill: Domain,
-    threeCheck: Domain,
-    antichess: Domain,
-    atomic: Domain,
-    horde: Domain,
-    racingKings: Domain,
-    crazyhouse: Domain)
+      standard: Domain,
+      chess960: Domain,
+      kingOfTheHill: Domain,
+      threeCheck: Domain,
+      antichess: Domain,
+      atomic: Domain,
+      horde: Domain,
+      racingKings: Domain,
+      crazyhouse: Domain) {
+
+    def apply(variant: Variant): Domain = variant match {
+      case Standard      => standard
+      case Chess960      => chess960
+      case KingOfTheHill => kingOfTheHill
+      case ThreeCheck    => threeCheck
+      case Antichess     => antichess
+      case Atomic        => atomic
+      case Horde         => horde
+      case RacingKings   => racingKings
+      case Crazyhouse    => crazyhouse
+    }
+  }
 }
