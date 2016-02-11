9ee54d9d99497194520465ddc0088c63f6ec489c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-02-11 23:40:52 +0100

    Use slightly more efficient contains check

--- app/GameInfoDatabase.scala
+++ app/GameInfoDatabase.scala
@@ -27,8 +27,7 @@ final class GameInfoDatabase extends MasterDatabasePacker {
   def get(gameId: String): Option[GameInfo] =
     Option(db.get(gameId)) flatMap GameInfoDatabase.unpack
 
-  def contains(gameId: String): Boolean =
-    Option(db.get(gameId)).isDefined
+  def contains(gameId: String): Boolean = db.exists(gameId)
 
   def store(gameId: String, info: GameInfo) =
     db.set(gameId, GameInfoDatabase pack info)
