debb8501c6e6ead7705f46fa1f72c76a229988f4
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2016-02-20 11:23:54 +0100

    Add failing test: Save some top games per speed

--- test/LichessDatabasePackerTest.scala
+++ test/LichessDatabasePackerTest.scala
@@ -76,6 +76,47 @@ class LichessDatabasePackerTest extends Specification with LichessDatabasePacker
       restored.totalAverageRatingSum mustEqual 864197252500L
     }
 
+    "save some top games per speed group" in {
+      val topGame = GameRef("hgfedcba", None, SpeedGroup.Classical, 2555)
+
+      // other classical games
+      val recent1 = GameRef("recent01", None, SpeedGroup.Classical, 2501)
+      val recent2 = GameRef("recent02", None, SpeedGroup.Classical, 2502)
+      val recent3 = GameRef("recent03", None, SpeedGroup.Classical, 2503)
+      val recent4 = GameRef("recent04", Some(Color.Black), SpeedGroup.Classical, 2504)
+      val recent5 = GameRef("recent05", None, SpeedGroup.Classical, 2505)
+      val recent6 = GameRef("recent06", None, SpeedGroup.Classical, 2506)
+      val recent7 = GameRef("recent07", Some(Color.White), SpeedGroup.Classical, 2507)
+      val recent8 = GameRef("recent08", None, SpeedGroup.Classical, 2508)
+      val recent9 = GameRef("recent09", None, SpeedGroup.Classical, 2509)
+
+      // higher ratings, but bullet
+      val better1 = GameRef("better01", Some(Color.Black), SpeedGroup.Bullet, 2777)
+      val better2 = GameRef("better02", Some(Color.Black), SpeedGroup.Bullet, 2778)
+      val better3 = GameRef("better03", Some(Color.Black), SpeedGroup.Bullet, 2779)
+      val better4 = GameRef("better04", Some(Color.White), SpeedGroup.Bullet, 2780)
+      val better5 = GameRef("better05", Some(Color.White), SpeedGroup.Bullet, 2781)
+      val better6 = GameRef("better06", None, SpeedGroup.Bullet, 2782)
+      val better7 = GameRef("better07", None, SpeedGroup.Bullet, 2783)
+      val better8 = GameRef("better08", Some(Color.White), SpeedGroup.Bullet, 2784)
+      val better9 = GameRef("better08", Some(Color.White), SpeedGroup.Bullet, 2785)
+
+      val entry = new Entry(Map(
+        (RatingGroup.Group2500, SpeedGroup.Classical) -> new SubEntry(
+          12345L, 23456L, 34567L, 456789L,
+          List(recent1, recent2, recent3, topGame, recent4, recent5, recent6, recent7, recent8, recent9)
+        ),
+        (RatingGroup.Group2500, SpeedGroup.Bullet) -> new SubEntry(
+          54321L, 65432L, 76543L, 98765L,
+          List(better1, better2, better3, better4, better5, better6, better7, better8, better9)
+        )
+      ))
+
+      val restored = unpack(pack(entry))
+
+      restored.allGameRefs must contain(topGame)
+    }
+
   }
 
 }
