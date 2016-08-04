2880d43c06582da324160f00a0034da4601b134a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-08-04 12:51:19 +0200

    exhaustive move generation is no longer used

--- app/Util.scala
+++ app/Util.scala
@@ -20,40 +20,6 @@ object Util {
     }
   }
 
-  def situationMoves(situation: chess.Situation): List[chess.Move] = {
-    // deduplicate castling moves
-    situation.moves.values.flatten.foldLeft(List.empty[chess.Move] -> Set.empty[chess.Pos]) {
-      case ((list, seenRooks), move) => move.castle match {
-        case Some((_, (rookPos, _))) =>
-          if (seenRooks(rookPos)) (list, seenRooks)
-          else (move :: list, seenRooks + rookPos)
-        case _ => (move :: list, seenRooks)
-      }
-    }._1.flatMap { move =>
-      move :: (
-        if (move.promotes)
-          // expand underpromotions
-          List(
-            move.withPromotion(chess.Knight.some),
-            move.withPromotion(chess.Bishop.some),
-            move.withPromotion(chess.Rook.some)
-          ).flatten
-        else Nil
-      )
-    }
-  }
-
-  def situationDrops(situation: chess.Situation): List[chess.Drop] = {
-    val droppablePositions = situation.drops.getOrElse(chess.Pos.all filterNot situation.board.pieces.contains)
-    (for {
-      role <- situation.board.crazyData.map(_.pockets(situation.color).roles.distinct).getOrElse(List.empty)
-      pos <- droppablePositions
-    } yield situation.drop(role, pos).toOption).flatten
-  }
-
-  def situationMovesOrDrops(situation: chess.Situation): List[chess.MoveOrDrop] =
-    situationMoves(situation).map(Left(_)) ::: situationDrops(situation).map(Right(_))
-
   def distinctHashes(hashes: List[chess.PositionHash]): Array[chess.PositionHash] =
     hashes.map(h => (h: WrappedArray[Byte])).distinct.map(_.array).toArray
 
--- test/UtilTest.scala
+++ test/UtilTest.scala
@@ -10,26 +10,6 @@ class UtilTest extends Specification {
 
   "Util" should {
 
-    "generate drops" in {
-      val fen = "r3k2r/pppq1ppp/2n1p3/3n2B1/3P4/4PP2/PP1QBP1P/R3K1R1/BPBpnn w Qkq - 22 12"
-      val situation = ((Forsyth << fen) get) withVariant Crazyhouse
-
-      val drop = situation.drop(Pawn, Pos.C3).toOption.get
-
-      Util.situationDrops(situation) must contain(drop)
-    }
-
-    "generate underpromotions" in {
-      val fen = "rnbqk1nr/ppp2ppp/8/4P3/1BP5/8/PP2KpPP/RN1Q1BNR b kq - 1 7"
-      val situation = ((Forsyth << fen) get) withVariant Standard
-
-      val fxg1N = situation.move(Pos.F2, Pos.G1, Some(Knight)).toOption.get
-      val fxg1Q = situation.move(Pos.F2, Pos.G1, Some(Queen)).toOption.get
-
-      Util.situationMoves(situation) must contain(fxg1N)
-      Util.situationMoves(situation) must contain(fxg1Q)
-    }
-
     "uniquify hashes" in {
       val a = Array(1.toByte, 2.toByte, 3.toByte)
       val b = a.toList.toArray
