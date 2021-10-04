46ff632a1f5e12f6a0e5aa4f6e45a13c51df1eae
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-04 18:25:37 +0200

    define some structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use shakmaty::Color;
+
+enum Speed {
+    Ultrabullet,
+    Bullet,
+    Blitz,
+    Rapid,
+    Classical,
+    Correspondence,
+}
+
+enum PerfType {
+    Rated,
+    Casual,
+}
+
+struct HashKey {
+    pos: (),
+    player: String,
+    color: Color,
+}
+
 fn main() {
     println!("Hello, world!");
 }
