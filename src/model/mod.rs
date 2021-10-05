b98197439ee25b5e691ebbb4291d16369aa616b5
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-05 15:49:47 +0200

    implement uci serialization

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,7 +1,10 @@
 mod game_id;
 
-use byteorder::{ReadBytesExt as _, WriteBytesExt as _};
+use byteorder::{LittleEndian, ReadBytesExt as _, WriteBytesExt as _};
 use std::io::{self, Read, Write};
+use std::convert::TryFrom;
+use shakmaty::{Role, Square};
+use shakmaty::uci::Uci;
 
 pub use game_id::{GameId, InvalidGameId};
 
@@ -25,6 +28,30 @@ fn write_uint<W: Write>(writer: &mut W, mut n: u64) -> io::Result<()> {
         n >>= 7;
     }
     writer.write_u8(n as u8)
+}
+
+fn read_uci<R: Read>(reader: &mut R) -> io::Result<Uci> {
+    let n = reader.read_u16::<LittleEndian>()?;
+    let from = Square::try_from(n & 63).unwrap();
+    let to = Square::try_from((n >> 6) & 63).unwrap();
+    let role = Role::try_from(n >> 12).ok();
+    Ok(if from == to {
+        match role {
+            Some(role) => Uci::Put { role, to },
+            None => Uci::Null,
+        }
+    } else {
+        Uci::Normal { from, to, promotion: role }
+    })
+}
+
+fn write_uci<W: Write>(writer: &mut W, uci: Uci) -> io::Result<()> {
+    let (from, to, role) = match uci {
+        Uci::Normal { from, to, promotion } => (from, to, promotion),
+        Uci::Put { role, to } => (to, to, Some(role)),
+        Uci::Null => (Square::A1, Square::A1, None),
+    };
+    writer.write_u16::<LittleEndian>(u16::from(from) | (u16::from(to) << 6) | role.map(u16::from).unwrap_or_default() << 12)
 }
 
 #[cfg(test)]
