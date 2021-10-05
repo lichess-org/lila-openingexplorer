cc4ceb40474086c7c6aa3180c57e1b030b4024e5
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-05 16:49:48 +0200

    move uint to its own module

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,4 +1,5 @@
 mod game_id;
+mod uint;
 
 use byteorder::{LittleEndian, ReadBytesExt as _, WriteBytesExt as _};
 use shakmaty::uci::Uci;
@@ -7,28 +8,6 @@ use std::convert::TryFrom;
 use std::io::{self, Read, Write};
 
 pub use game_id::{GameId, InvalidGameId};
-
-fn read_uint<R: Read>(reader: &mut R) -> io::Result<u64> {
-    let mut n = 0;
-    for shift in (0..).step_by(7) {
-        let byte = reader.read_u8()?;
-        n |= u64::from(byte & 127)
-            .checked_shl(shift)
-            .ok_or_else(|| io::Error::from(io::ErrorKind::InvalidData))?;
-        if byte & 128 == 0 {
-            break;
-        }
-    }
-    Ok(n)
-}
-
-fn write_uint<W: Write>(writer: &mut W, mut n: u64) -> io::Result<()> {
-    while n > 127 {
-        writer.write_u8((n as u8 & 127) | 128)?;
-        n >>= 7;
-    }
-    writer.write_u8(n as u8)
-}
 
 fn read_uci<R: Read>(reader: &mut R) -> io::Result<Uci> {
     let n = reader.read_u16::<LittleEndian>()?;
@@ -98,16 +77,6 @@ mod tests {
         let mut reader = Cursor::new(writer.into_inner());
         for uci in moves {
             assert_eq!(uci, read_uci(&mut reader).unwrap());
-        }
-    }
-
-    quickcheck! {
-        fn uint_roundtrip(n: u64) -> bool {
-            let mut writer = Cursor::new(Vec::new());
-            write_uint(&mut writer, n).unwrap();
-
-            let mut reader = Cursor::new(writer.into_inner());
-            read_uint(&mut reader).unwrap() == n
         }
     }
 }
---
+++ src/model/uint.rs
@@ -0,0 +1,41 @@
+use std::io::{self, Read, Write};
+use byteorder::{ReadBytesExt as _, WriteBytesExt as _};
+
+fn read_uint<R: Read>(reader: &mut R) -> io::Result<u64> {
+    let mut n = 0;
+    for shift in (0..).step_by(7) {
+        let byte = reader.read_u8()?;
+        n |= u64::from(byte & 127)
+            .checked_shl(shift)
+            .ok_or_else(|| io::Error::from(io::ErrorKind::InvalidData))?;
+        if byte & 128 == 0 {
+            break;
+        }
+    }
+    Ok(n)
+}
+
+fn write_uint<W: Write>(writer: &mut W, mut n: u64) -> io::Result<()> {
+    while n > 127 {
+        writer.write_u8((n as u8 & 127) | 128)?;
+        n >>= 7;
+    }
+    writer.write_u8(n as u8)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quickcheck::quickcheck;
+    use std::io::Cursor;
+
+    quickcheck! {
+        fn uint_roundtrip(n: u64) -> bool {
+            let mut writer = Cursor::new(Vec::new());
+            write_uint(&mut writer, n).unwrap();
+
+            let mut reader = Cursor::new(writer.into_inner());
+            read_uint(&mut reader).unwrap() == n
+        }
+    }
+}
