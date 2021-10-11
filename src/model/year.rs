be454dad3e0c0a8e5dc9d2c7d8ec031b238644a8
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-11 15:46:03 +0200

    forgot to commit year.rs

---
+++ src/model/year.rs
@@ -0,0 +1,20 @@
+use chrono::{DateTime, Datelike as _, Utc};
+use std::convert::TryInto;
+
+#[derive(Debug, Copy, Clone)]
+pub struct AnnoLichess(pub u8);
+
+impl AnnoLichess {
+    pub const MAX: AnnoLichess = AnnoLichess(u8::MAX);
+
+    pub fn from_year(year: u32) -> AnnoLichess {
+        AnnoLichess(year.saturating_sub(2000).try_into().unwrap_or(u8::MAX))
+    }
+
+    pub fn from_time(time: DateTime<Utc>) -> AnnoLichess {
+        match time.year_ce() {
+            (true, ce) => AnnoLichess::from_year(ce),
+            (false, _) => AnnoLichess(0),
+        }
+    }
+}
