50234723a423ef97be9b99bc07524ea63717e219
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-26 20:55:38 +0200

    update test after refactoring

--- src/model/key.rs
+++ src/model/key.rs
@@ -98,7 +98,7 @@ mod tests {
     quickcheck! {
         fn test_key_order(a: Month, b: Month) -> bool {
             let user_id = UserId::from("blindfoldpig".parse::<UserName>().unwrap());
-            let prefix = KeyBuilder::personal(&user_id, Color::White)
+            let prefix = KeyBuilder::player(&user_id, Color::White)
                 .with_zobrist(Variant::Chess, 0xd1d06239bd7d2ae8ad6fa208133e1f9a);
 
             (a <= b) == (prefix.with_month(a).into_bytes() <= prefix.with_month(b).into_bytes())
