97bb7012f7f7a10ad1469916eefc369aab156aa9
Niklas Fiekas <niklas.fiekas@backscattering.de>
2023-02-28 20:33:48 +0100

    async io tail latencies are higher under congestion

--- src/db.rs
+++ src/db.rs
@@ -15,8 +15,6 @@ use crate::{
     },
 };
 
-const ASYNC_IO: bool = true;
-
 #[derive(Parser)]
 pub struct DbOpt {
     /// Path to RocksDB database.
@@ -297,7 +295,6 @@ impl MastersDatabase<'_> {
     ) -> Result<Vec<Option<MastersGame>>, rocksdb::Error> {
         let mut opt = ReadOptions::default();
         opt.set_ignore_range_deletions(true);
-        opt.set_async_io(ASYNC_IO);
         self.inner
             .batched_multi_get_cf_opt(
                 self.cf_masters_game,
@@ -331,7 +328,6 @@ impl MastersDatabase<'_> {
 
         let mut opt = ReadOptions::default();
         opt.set_ignore_range_deletions(true);
-        opt.set_async_io(ASYNC_IO);
         opt.set_prefix_same_as_start(true);
         opt.set_iterate_lower_bound(key.with_year(since).into_bytes());
         opt.set_iterate_upper_bound(key.with_year(until.add_years_saturating(1)).into_bytes());
@@ -444,7 +440,6 @@ impl LichessDatabase<'_> {
     ) -> Result<Vec<Option<LichessGame>>, rocksdb::Error> {
         let mut opt = ReadOptions::default();
         opt.set_ignore_range_deletions(true);
-        opt.set_async_io(ASYNC_IO);
         self.inner
             .batched_multi_get_cf_opt(
                 self.cf_lichess_game,
@@ -470,7 +465,6 @@ impl LichessDatabase<'_> {
 
         let mut opt = ReadOptions::default();
         opt.set_ignore_range_deletions(true);
-        opt.set_async_io(ASYNC_IO);
         opt.set_prefix_same_as_start(true);
         opt.set_iterate_lower_bound(
             key.with_month(since.unwrap_or_else(Month::min_value))
@@ -502,7 +496,6 @@ impl LichessDatabase<'_> {
 
         let mut opt = ReadOptions::default();
         opt.set_ignore_range_deletions(true);
-        opt.set_async_io(ASYNC_IO);
         opt.set_prefix_same_as_start(true);
         opt.set_iterate_lower_bound(
             key.with_month(filter.since.unwrap_or_else(Month::min_value))
@@ -561,7 +554,6 @@ impl LichessDatabase<'_> {
 
         let mut opt = ReadOptions::default();
         opt.set_ignore_range_deletions(true);
-        opt.set_async_io(ASYNC_IO);
         opt.set_prefix_same_as_start(true);
         opt.set_iterate_lower_bound(key.with_month(since).into_bytes());
         opt.set_iterate_upper_bound(key.with_month(until.add_months_saturating(1)).into_bytes());
